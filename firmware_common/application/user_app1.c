550948f131d962c1e3f7556134663f42a44db683
James Bootsma <jrbootsma@gmail.com>
2023-11-30 18:57:03 -0700

    Completed the exercise

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -1,5 +1,5 @@
 /*!*********************************************************************************************************************
-@file user_app1.c                                                                
+@file user_app1.c
 @brief User's tasks / applications are written here.  This description
 should be replaced by something specific to the task.
 
@@ -7,14 +7,21 @@ should be replaced by something specific to the task.
 To start a new task using this user_app1 as a template:
  1. Copy both user_app1.c and user_app1.h to the Application directory
  2. Rename the files yournewtaskname.c and yournewtaskname.h
- 3. Add yournewtaskname.c and yournewtaskname.h to the Application Include and Source groups in the IAR project
- 4. Use ctrl-h (make sure "Match Case" is checked) to find and replace all instances of "user_app1" with "yournewtaskname"
- 5. Use ctrl-h to find and replace all instances of "UserApp1" with "YourNewTaskName"
- 6. Use ctrl-h to find and replace all instances of "USER_APP1" with "YOUR_NEW_TASK_NAME"
+ 3. Add yournewtaskname.c and yournewtaskname.h to the Application Include and
+Source groups in the IAR project
+ 4. Use ctrl-h (make sure "Match Case" is checked) to find and replace all
+instances of "user_app1" with "yournewtaskname"
+ 5. Use ctrl-h to find and replace all instances of "UserApp1" with
+"YourNewTaskName"
+ 6. Use ctrl-h to find and replace all instances of "USER_APP1" with
+"YOUR_NEW_TASK_NAME"
  7. Add a call to YourNewTaskNameInitialize() in the init section of main
- 8. Add a call to YourNewTaskNameRunActiveState() in the Super Loop section of main
- 9. Update yournewtaskname.h per the instructions at the top of yournewtaskname.h
-10. Delete this text (between the dashed lines) and update the Description below to describe your task
+ 8. Add a call to YourNewTaskNameRunActiveState() in the Super Loop section of
+main
+ 9. Update yournewtaskname.h per the instructions at the top of
+yournewtaskname.h
+10. Delete this text (between the dashed lines) and update the Description below
+to describe your task
 ----------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -44,35 +51,35 @@ Global variable definitions with scope across entire project.
 All Global variable names shall start with "G_<type>UserApp1"
 ***********************************************************************************************************************/
 /* New variables */
-volatile u32 G_u32UserApp1Flags;                          /*!< @brief Global state flags */
-
+volatile u32 G_u32UserApp1Flags; /*!< @brief Global state flags */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* Existing variables (defined in other files -- should all contain the "extern" keyword) */
-extern volatile u32 G_u32SystemTime1ms;                   /*!< @brief From main.c */
-extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.c */
-extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
-extern volatile u32 G_u32ApplicationFlags;                /*!< @brief From main.c */
-
+/* Existing variables (defined in other files -- should all contain the "extern"
+ * keyword) */
+extern volatile u32 G_u32SystemTime1ms;    /*!< @brief From main.c */
+extern volatile u32 G_u32SystemTime1s;     /*!< @brief From main.c */
+extern volatile u32 G_u32SystemFlags;      /*!< @brief From main.c */
+extern volatile u32 G_u32ApplicationFlags; /*!< @brief From main.c */
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "UserApp1_<type>" and be declared as static.
 ***********************************************************************************************************************/
-static fnCode_type UserApp1_pfStateMachine;               /*!< @brief The state machine function pointer */
-//static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout counter used across states */
-
+static fnCode_type
+    UserApp1_pfStateMachine; /*!< @brief The state machine function pointer */
+// static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout
+// counter used across states */
 
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/*! @publicsection */                                                                                            
+/*! @publicsection */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/*! @protectedsection */                                                                                            
+/*! @protectedsection */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /*!--------------------------------------------------------------------------------------------------------------------
@@ -90,29 +97,34 @@ Promises:
 - NONE
 
 */
-void UserApp1Initialize(void)
-{
+void UserApp1Initialize(void) {
+  LcdCommand(LCD_CLEAR_CMD);
+
+  LcdMessage(LINE1_START_ADDR, "Hi, I'm James");
+  LcdMessage(LINE2_START_ADDR, "1");
+  LcdMessage(LINE2_START_ADDR + 6, "2");
+  LcdMessage(LINE2_START_ADDR + 13, "3");
+  LcdMessage(LINE2_START_ADDR + 19, "4");
+
+  LcdCommand(LCD_HOME_CMD);
+
   /* If good initialization, set state to Idle */
-  if( 1 )
-  {
+  if (1) {
     UserApp1_pfStateMachine = UserApp1SM_Idle;
-  }
-  else
-  {
+  } else {
     /* The task isn't properly initialized, so shut it down and don't run */
     UserApp1_pfStateMachine = UserApp1SM_Error;
   }
 
 } /* end UserApp1Initialize() */
 
-  
 /*!----------------------------------------------------------------------------------------------------------------------
 @fn void UserApp1RunActiveState(void)
 
 @brief Selects and runs one iteration of the current state in the state machine.
 
-All state machines have a TOTAL of 1ms to execute, so on average n state machines
-may take 1ms / n to execute.
+All state machines have a TOTAL of 1ms to execute, so on average n state
+machines may take 1ms / n to execute.
 
 Requires:
 - State machine function pointer points at current state
@@ -121,39 +133,41 @@ Promises:
 - Calls the function to pointed by the state machine function pointer
 
 */
-void UserApp1RunActiveState(void)
-{
+void UserApp1RunActiveState(void) {
   UserApp1_pfStateMachine();
 
 } /* end UserApp1RunActiveState */
 
-
 /*------------------------------------------------------------------------------------------------------------------*/
-/*! @privatesection */                                                                                            
+/*! @privatesection */
 /*--------------------------------------------------------------------------------------------------------------------*/
-
 
 /**********************************************************************************************************************
 State Machine Function Definitions
 **********************************************************************************************************************/
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* What does this state do? */
-static void UserApp1SM_Idle(void)
-{
-    
+static void UserApp1SM_Idle(void) {
+  static bool bCurserOn = FALSE;
+
+  if (WasButtonPressed(BUTTON0)) {
+    ButtonAcknowledge(BUTTON0);
+
+    bCurserOn = !bCurserOn;
+
+    u8 u8Cmd = LCD_DISPLAY_CMD | LCD_DISPLAY_ON;
+    if (bCurserOn) {
+      u8Cmd |= LCD_DISPLAY_CURSOR | LCD_DISPLAY_BLINK;
+    }
+
+    LcdCommand(u8Cmd);
+  }
 } /* end UserApp1SM_Idle() */
-     
 
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Handle an error */
-static void UserApp1SM_Error(void)          
-{
-  
-} /* end UserApp1SM_Error() */
-
-
-
+static void UserApp1SM_Error(void) {} /* end UserApp1SM_Error() */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* End of File                                                                                                        */
+/* End of File */
 /*--------------------------------------------------------------------------------------------------------------------*/
