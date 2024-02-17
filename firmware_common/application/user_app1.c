0089fc4eac9fe516c7892f7590614d69ab913531
James Bootsma <jrbootsma@gmail.com>
2024-02-17 10:43:34 -0700

    Use new usb driver API + Add WINUSB compat
    Updates the demo code to use the new USB driver API. Also add the
    appropriate descriptors to declare compatibility with the WINUSB driver
    so that manual driver install does not need to be done on windows.

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -52,8 +52,8 @@ PROTECTED FUNCTIONS
 // #define USB_VID
 // #define USB_PID
 
-#define EP_IN 1
-#define EP_OUT 2
+#define EP_WRITE 1
+#define EP_READ 2
 
 /***********************************************************************************************************************
 Global variable definitions with scope across entire project.
@@ -91,8 +91,7 @@ Function Definitions
 **********************************************************************************************************************/
 
 static void SetLedStatus(LedNameType eLed_);
-static void HandleDevEvt(UsbDevEvtIdType eEvt_);
-static void HandleIfaceEvt(UsbIfaceEvtIdType eEvt_);
+static void HandleUsbEvent(UsbEventIdType eEvt_);
 static void LcdNumber(u8 u8Address, u32 u32Num);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -102,6 +101,118 @@ static void LcdNumber(u8 u8Address, u32 u32Num);
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @protectedsection */
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+#define NTDDI_WIN8 0x06020000
+
+#define MS_VENDOR_CODE 0x01
+
+typedef enum {
+  STRING_MFG = 1,
+  STRING_PROD,
+  STRING_SERIAL,
+} UsbStringType;
+
+static const UsbDriverConfigType stDriverCfg = {
+    .pfnEventHandler = HandleUsbEvent,
+    .stFullSpeedEp0Cfg =
+        {
+            .u16MaxPacketSize = 64,
+            .u8NumPackets = 1,
+        },
+};
+
+static const UsbDevDescType stDevDesc = {
+    .stHeader = USB_DEV_DESC_HEADER,
+    .stUsbVersion =
+        {
+            .u8Major = 2,
+            .u4Patch = 1,
+        },
+    .stDevClass = {.u8Class = USB_CLASS_VENDOR_SPECIFIC},
+    .u8Ep0PktSize = 64,
+    .u16Vid = USB_VID,
+    .u16Pid = USB_PID,
+    .stDevVersion =
+        {
+            .u4Minor = 1,
+        },
+    .u8ManufacturerStr = STRING_MFG,
+    .u8ProductStr = STRING_PROD,
+    .u8SerialStr = STRING_SERIAL,
+    .u8NumCfgs = 1,
+};
+
+static UsbCfgDescType stMainCfg = {
+    .stHeader = USB_CFG_DESC_HEADER,
+    .u8NumIfaces = 1,
+    .u8CfgIdx = 1,
+    .u8MaxPower = 50,
+    .stAttrib = {._deprecated = 1},
+};
+
+static const UsbIfaceDescType stIfaceDesc = {
+    .stHeader = USB_IFACE_DESC_HEADER,
+    .u8IfaceIdx = 0,
+    .u8AltIdx = 0,
+    .u8NumEpts = 2,
+    .stIfaceClass = {.u8Class = USB_CLASS_VENDOR_SPECIFIC},
+};
+
+static const UsbEptDescType stWriteEpt = {
+    .stHeader = USB_EPT_DESC_HEADER,
+    .stAddress = {.u4EptNum = EP_WRITE, .eDir = USB_EPT_DIR_IN},
+    .stAttrib = {.eXfer = USB_XFER_BULK},
+    .u16MaxPktSize = 64,
+};
+
+static const UsbEptDescType stReadEpt = {
+    .stHeader = USB_EPT_DESC_HEADER,
+    .stAddress = {.u4EptNum = EP_READ, .eDir = USB_EPT_DIR_OUT},
+    .stAttrib = {.eXfer = USB_XFER_BULK},
+    .u16MaxPktSize = 64,
+};
+
+static const UsbDescListType stMainCfgDescs =
+    MAKE_USB_DESC_LIST(&stMainCfg, &stIfaceDesc, &stWriteEpt, &stReadEpt);
+
+static const UsbEndpointConfigType astEptCfgs[] = {
+    {
+        .eDir = USB_EPT_DIR_TO_HOST,
+        .eXferType = USB_XFER_BULK,
+        .u16MaxPacketSize = 64,
+        .u8NumPackets = 2,
+    },
+    {
+        .eDir = USB_EPT_DIR_FROM_HOST,
+        .eXferType = USB_XFER_BULK,
+        .u16MaxPacketSize = 64,
+        .u8NumPackets = 2,
+    }};
+
+static UsbBosDescType stBos = {
+    .stHeader = USB_BOS_DESC_HEADER,
+    .u8NumDeviceCaps = 1,
+};
+
+DECL_USB_MS_PLAT_CAPABILITY(stMsCap, {
+                                         .u32NtddiWinVersion = NTDDI_WIN8,
+                                         .u8VendorCode = MS_VENDOR_CODE,
+                                     });
+
+static const UsbDescListType stBosDescs = MAKE_USB_DESC_LIST(&stBos, &stMsCap);
+
+static UsbMsOs20DescSetHeaderType stMsDescHeader = {
+    .stHeader = USB_MS_OS_20_SET_HEADER_DESCRIPTOR_HEADER,
+    .u32NtddiWindowsVersion = NTDDI_WIN8,
+};
+
+static const UsbMsOs20CompatitbleIdType stCompatId = {
+    .stHeader = USB_MS_OS_20_FEATURE_COMPATBLE_ID_HEADER,
+    .stCompatId = "WINUSB",
+};
+
+static const UsbMsOs20DescListType stMsDescs =
+    MAKE_USB_MS_OS_20_DESC_LIST(&stMsDescHeader, &stCompatId);
 
 /*!--------------------------------------------------------------------------------------------------------------------
 @fn void UserApp1Initialize(void)
@@ -119,67 +230,6 @@ Promises:
 
 */
 void UserApp1Initialize(void) {
-  bool bCfgOk = TRUE;
-
-  static const UsbDeviceInfoType stDevInfo = {
-      .pcManufacturerName = "Embedded in Embedded",
-      .pcProductName = "Razor Dev Board",
-      .pcSerialNum = "12345678",
-
-      .pfnEventHandler = HandleDevEvt,
-
-      .stId =
-          {
-              .u16Vendor = USB_VID,
-              .u16Product = USB_PID,
-          },
-
-      .stDevVersion =
-          {
-              .u8Major = 0,
-              .u8Minor = 1,
-              .u8SubMinor = 0,
-          },
-  };
-
-  bCfgOk = bCfgOk && UsbSetDeviceInfo(&stDevInfo);
-
-  static const UsbConfigInfoType stCfgInfo = {
-      .pcConfigName = "Usb Demo",
-  };
-
-  bCfgOk = bCfgOk && UsbAddConfig(&stCfgInfo);
-
-  // Two endpoints for echo usage, 64 is max packet size at full speed.
-  // Remember In is dev->host.
-
-  static const UsbEndptInfoType stEpInCfg = {
-      .bIsIn = TRUE,
-      .u8TransferType = USB_XFER_TYPE_BULK,
-      .u16PacketSize = 64,
-  };
-
-  static const UsbEndptInfoType stEpOutCfg = {
-      .bIsIn = FALSE,
-      .u8TransferType = USB_XFER_TYPE_BULK,
-      .u16PacketSize = 64,
-  };
-
-  static const UsbIfaceInfoType stIfaceInfo = {
-      .pcIfaceName = "Bulk Data Echo",
-      .pfnEventHandler = HandleIfaceEvt,
-      .stClass =
-          {
-              .u8Class = USB_CLASS_VENDOR_SPECIFIC,
-          },
-  };
-
-  bCfgOk = bCfgOk && UsbAddIface(&stIfaceInfo, FALSE);
-  bCfgOk = bCfgOk && UsbSetEndpointCapacity(EP_IN, 64, 2);
-  bCfgOk = bCfgOk && UsbSetEndpointCapacity(EP_OUT, 64, 2);
-  bCfgOk = bCfgOk && UsbUseEndpt(EP_IN, &stEpInCfg);
-  bCfgOk = bCfgOk && UsbUseEndpt(EP_OUT, &stEpOutCfg);
-
   LedOn(LCD_RED);
   LedOn(LCD_GREEN);
   LedOn(LCD_BLUE);
@@ -187,6 +237,14 @@ void UserApp1Initialize(void) {
   LcdCommand(LCD_HOME_CMD);
   LcdCommand(LCD_CLEAR_CMD);
   LcdCommand(LCD_DISPLAY_CMD | LCD_DISPLAY_ON);
+  LcdNumber(LINE1_START_ADDR, 0);
+
+  stMainCfg.u16TotLen = UsbDescListByteLen(stMainCfgDescs);
+  stBos.u16TotalLength = UsbDescListByteLen(stBosDescs);
+  stMsDescHeader.u16TotalLength = UsbMsOs20DescListByteLen(stMsDescs);
+  stMsCap.astDescSets[0].u16DescSetLength = stMsDescHeader.u16TotalLength;
+
+  bool bCfgOk = UsbSetDriverConfig(&stDriverCfg);
 
   /* If good initialization, set state to Idle */
   if (bCfgOk) {
@@ -231,11 +289,11 @@ State Machine Function Definitions
 static void UserApp1SM_Idle(void) {
   static bool attach_started = FALSE;
 
-  static u32 u32FrameCounter = 100;
-  if (--u32FrameCounter == 0) {
-    u32FrameCounter = 100;
+  static u32 u32Displayed = 0;
+  if (u32Displayed != u32EchoCount) {
     LcdClearChars(LINE1_START_ADDR, U8_LCD_MAX_LINE_DISPLAY_SIZE);
     LcdNumber(LINE1_START_ADDR, u32EchoCount);
+    u32Displayed = u32EchoCount;
   }
 
   if (WasButtonPressed(BUTTON0)) {
@@ -258,16 +316,16 @@ static void UserApp1SM_Idle(void) {
     attach_started = FALSE;
   }
 
-  if (UsbIsPacketReady(EP_OUT) && UsbIsPacketReady(EP_IN)) {
+  if (UsbIsPacketReady(EP_READ) && UsbIsPacketReady(EP_WRITE)) {
     static u8 pkt_buf[64];
 
-    u8 pkt_sz = UsbRead(EP_OUT, pkt_buf, 64);
-    UsbWrite(EP_IN, pkt_buf, pkt_sz);
+    u8 pkt_sz = UsbRead(EP_READ, pkt_buf, 64);
+    UsbWrite(EP_WRITE, pkt_buf, pkt_sz);
 
-    UsbNextPacket(EP_IN);
+    UsbNextPacket(EP_WRITE);
 
-    if (UsbGetPktOffset(EP_OUT) == UsbGetPktSize(EP_OUT)) {
-      UsbNextPacket(EP_OUT);
+    if (UsbGetPktOffset(EP_READ) == UsbGetPktSize(EP_READ)) {
+      UsbNextPacket(EP_READ);
     }
 
     u32EchoCount += pkt_sz;
@@ -281,32 +339,165 @@ static void UserApp1SM_Error(void) {
   UsbSetEnabled(FALSE);
 } /* end UserApp1SM_Error() */
 
-/* Process interface specific USB events. */
-static void HandleIfaceEvt(UsbIfaceEvtIdType eEvt_) {
-  switch (eEvt_) {
-  case USB_IFACE_EVT_SETUP:
-  case USB_IFACE_EVT_RESUME:
-    bIfaceActive = TRUE;
-    SetLedStatus(GREEN);
+static void HandleStringRequest(UsbStringType eString) {
+  const char *pcStr = NULL;
+
+  switch (eString) {
+  case STRING_MFG:
+    pcStr = "James Bootsma";
     break;
 
-  case USB_IFACE_EVT_TEARDOWN:
-  case USB_IFACE_EVT_SUSPEND:
-    bIfaceActive = FALSE;
-    SetLedStatus(YELLOW);
+  case STRING_PROD:
+    pcStr = "Razor Dev Board";
     break;
+
+  case STRING_SERIAL:
+    pcStr = "1234576890";
+    break;
+  }
+
+  if (pcStr) {
+    void *desc = UsbCreateStringDesc(pcStr);
+    if (desc) {
+      UsbAcceptRequest(UsbSendDesc, free, desc);
+    }
+  }
+}
+
+static void HandleStandardRequest(const UsbSetupPacketType *pstRequest) {
+  UsbStandardRequestIdType eId = pstRequest->u8RequestId;
+
+  switch (eId) {
+  case USB_REQ_GET_DESCRIPTOR: {
+    UsbDescType eType = (u8)(pstRequest->u16Value >> 8);
+    u8 u8Idx = (u8)pstRequest->u16Value;
+
+    switch (eType) {
+    case USB_DESC_TYPE_DEV:
+      if (u8Idx == 0) {
+        UsbAcceptRequest(UsbSendDesc, NULL, (void *)&stDevDesc);
+      }
+      break;
+
+    case USB_DESC_TYPE_CFG:
+      if (u8Idx == 0) {
+        UsbAcceptRequest(UsbSendDescList, NULL, (void *)&stMainCfgDescs);
+      }
+      break;
+
+    case USB_DESC_TYPE_STRING: {
+      if (u8Idx == 0) {
+        u16 u16Lang = USB_LANG_ID_EN_US;
+        UsbDescHeaderType *pstDesc = UsbCreateLangIds(1, &u16Lang);
+        if (pstDesc) {
+          UsbAcceptRequest(UsbSendDesc, free, pstDesc);
+        }
+      } else {
+        HandleStringRequest(u8Idx);
+      }
+    } break;
+
+    case USB_DESC_TYPE_BOS:
+      if (u8Idx == 0) {
+        UsbAcceptRequest(UsbSendDescList, NULL, (void *)&stBosDescs);
+      }
+      break;
+
+    default:
+      break;
+    }
+
+  } break;
+
+  case USB_REQ_GET_CFG: {
+    u8 u8Cfg = 0;
+    if (bIfaceActive) {
+      u8Cfg = 1;
+    }
+    UsbWrite(USB_DEF_CTRL_EP, &u8Cfg, 1);
+    UsbNextPacket(USB_DEF_CTRL_EP);
+  } break;
+
+  case USB_REQ_SET_CFG: {
+    u8 u8Cfg = (u8)pstRequest->u16Value;
+    bool bSuccess = FALSE;
+
+    switch (u8Cfg) {
+    case 0:
+      bSuccess = UsbSetEndpointsConfig(0, NULL);
+      if (bSuccess) {
+        SetLedStatus(YELLOW);
+        bIfaceActive = FALSE;
+      }
+      break;
+
+    case 1:
+      bSuccess = UsbSetEndpointsConfig(2, astEptCfgs);
+      if (bSuccess) {
+        SetLedStatus(GREEN);
+        bIfaceActive = TRUE;
+      }
+      break;
+    }
+
+    if (bSuccess) {
+      UsbNextPacket(USB_DEF_CTRL_EP);
+    } else {
+      UsbFailRequest();
+    }
+  } break;
+
+  case USB_REQ_GET_IFACE: {
+    u8 u8Iface = 0;
+    UsbWrite(USB_DEF_CTRL_EP, &u8Iface, 1);
+    UsbNextPacket(USB_DEF_CTRL_EP);
+  } break;
+
+  case USB_REQ_SET_IFACE: {
+    if (pstRequest->u16Value == 0 && pstRequest->u16Index == 0) {
+      UsbNextPacket(USB_DEF_CTRL_EP);
+    } else {
+      UsbFailRequest();
+    }
+  } break;
 
   default:
     break;
   }
 }
 
+static void HandleMsRequest(const UsbSetupPacketType *pstRequest) {
+  switch (pstRequest->u16Index) {
+  case USB_MS_OS_20_DESCRIPTOR_INDEX:
+    UsbAcceptRequest(UsbSendMsOs20DescSet, NULL, (void *)&stMsDescs);
+    break;
+  }
+}
+
 /* Process device-level USB events. */
-static void HandleDevEvt(UsbDevEvtIdType eEvt_) {
+static void HandleUsbEvent(UsbEventIdType eEvt_) {
   switch (eEvt_) {
-  case USB_DEV_EVT_RESET:
+  case USB_EVT_RESET:
     SetLedStatus(YELLOW);
     break;
+
+  case USB_EVT_REQUEST: {
+    const UsbSetupPacketType *pstRequest = UsbGetCurrentRequest();
+    switch (pstRequest->stRequestType.eType) {
+    case USB_REQ_TYPE_STANDARD:
+      HandleStandardRequest(pstRequest);
+      break;
+
+    case USB_REQ_TYPE_VENDOR:
+      if (pstRequest->u8RequestId == MS_VENDOR_CODE) {
+        HandleMsRequest(pstRequest);
+      }
+      break;
+
+    default:
+      break;
+    }
+  } break;
 
   default:
     break;
